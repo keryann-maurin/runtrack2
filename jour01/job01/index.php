0267577b2c4c7ba3854cb2d461aa824d1397e423
Keryann Maurin <72126212+keryann-maurin@users.noreply.github.com>
2020-11-02 13:04:18 +0100

    Found the solution to only show "LaPlateforme"

---
+++ jour01/job01/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Runtrack PHP</title>
+        <meta charset="utf-8">
+    </head>
+    
+    <body>
+        <header>
+
+
+        </header>
+
+        <main>
+            <h1></h1>
+            <?php
+            $hello = "Hello ";
+            $lplt = "LaPlateforme";
+            $str = $hello .$lplt;
+            $str2 = "Vive ";
+            $str3 = "!";       
+            echo ($str2 .$lplt .$str3);
+            ?>
+        
+            <p>Comment allez vous aujourd'hui?</p>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
