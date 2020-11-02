49f08261a029474e4da7a2ab0ddf7485f7f7044d
Keryann Maurin <72126212+keryann-maurin@users.noreply.github.com>
2020-11-02 14:51:34 +0100

    Done the job

---
+++ jour01/job02/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Runtrack PHP</title>
+        <meta charset="utf-8">
+    </head>
+    
+    <body>
+        <header>
+
+
+        </header>
+
+        <main>
+            <h1>Job 2</h1>
+            <?php
+            $hello = "Hello ";
+            $lplt = "LaPlateforme";
+            $str = $hello .$lplt;
+            $str2 = "Vive ";
+            $str3 = "!";
+
+            echo $str. $str3. '<br>'; //Ceci est un commentaire en style c++
+            echo $str. $str3. '<br>'; #Ceci est un commentaire en style shell
+            ?>
+            
+            <p>Comment allez vous aujourd'hui?</p>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
