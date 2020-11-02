55f8b6ae0c4f822e6410c5709117a156755e49a1
Keryann Maurin <72126212+keryann-maurin@users.noreply.github.com>
2020-11-02 10:31:55 +0100

    Premier test PHP, simpliste. Job00

---
+++ jour01/job00/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Runtrack PHP</title>
+        <meta charset="utf-8">
+    </head>
+    
+    <body>
+        <header>
+
+
+        </header>
+
+        <main>
+            <h1>Une Petite Salutation personnalisée.</h1>
+            <?php
+            $hello = "Hello LaPlateforme!";
+                    
+            echo $hello;
+            ?>
+        
+            <p>Comment allez vous aujourd'hui?</p>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
